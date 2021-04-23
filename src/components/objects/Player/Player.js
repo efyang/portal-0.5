6a7693e66a07ccc51d438f13025cbc7ae6d7686b
Allen Dai <allendai@princeton.edu>
2021-04-23 10:02:51 -0400

    updated player movement

--- src/components/objects/Player/Player.js
+++ src/components/objects/Player/Player.js
@@ -19,63 +19,58 @@ class Player extends Group {
         var slipperyMaterial = new CANNON.Material();
         slipperyMaterial.friction = 0.01;
 
-        // let physicsMaterial = new CANNON.Material({friction: 0.0});
-        let sphereShape = new CANNON.Box(new CANNON.Vec3(0.5, 2, 0.5));
-        // let sphereShape = new CANNON.Sphere(1);
-        //this.sphereBody = new CANNON.Body({ mass: mass, material: physicsMaterial });
-        this.sphereBody = new CANNON.Body({ mass: mass, material: slipperyMaterial });
-        // this.sphereBody = new CANNON.Body({ mass: mass});
-        this.sphereBody.addShape(sphereShape);
-        this.sphereBody.position.set(0,5,0);
-        // this.sphereBody.linearDamping = 0.9;
-        this.sphereBody.linearDamping = 0.6;
-        // keep the player upright
-        this.sphereBody.angularDamping = 1
-        this.sphereBody.inJump = false
-        this.sphereBody.updateMassProperties()
-        parent.state.cworld.addBody(this.sphereBody);
+        var regularMaterial = new CANNON.Material();
+        regularMaterial.friction = 0.3;
 
-        let sphereBody = this.sphereBody
+        // define shape
+        let physicsShape = new CANNON.Box(new CANNON.Vec3(0.5, 2, 0.5)); 
+
+        // define the physical body attributes
+        this.physicsBody = new CANNON.Body({ mass: mass, material: slipperyMaterial });
+        // this.physicsBody = new CANNON.Body({ mass: mass, material: regularMaterial });
+        this.physicsBody.addShape(physicsShape);
+        this.physicsBody.position.set(0,5,0);
+        this.physicsBody.linearDamping = 0.6;
+
+        // keep the player upright
+        this.physicsBody.angularDamping = 1
+        this.physicsBody.inJump = false
+        this.physicsBody.updateMassProperties()
+        parent.state.cworld.addBody(this.physicsBody);
+
+        let physicsBody = this.physicsBody
         parent.addToUpdateList(this);
 
-        // setup ev handler
-        window.addEventListener("keydown", (e) => this.handleKeypress(e), false)
-
-        this.sphereBody.addEventListener("collide", function(e){ 
+        this.physicsBody.addEventListener("collide", function(e){ 
             let EPS = 0.2
-            if (Math.abs(sphereBody.velocity.y) > EPS) {return}
+            if (Math.abs(physicsBody.velocity.y) > EPS) {return}
             this.inJump = false
         } );
+
+        // for movement
+        this.controller = {
+            "KeyW": {pressed: false},
+            "KeyS": {pressed: false},
+            "KeyA": {pressed: false},
+            "KeyD": {pressed: false},
+            "Space": {pressed: false},
+        }
+
+        window.addEventListener("keydown", (e) => {
+            if(this.controller[e.code]){
+                this.controller[e.code].pressed = true;
+            }
+        })
+        window.addEventListener("keyup", (e) => {
+            if(this.controller[e.code]){
+                this.controller[e.code].pressed = false;
+            }
+        })
+
     }
 
     update(timeStamp) {
-        let v = this.sphereBody.velocity.clone()
-        // always look where the camera points
-        this.sphereBody.quaternion.copy(window.camera.quaternion)
-        this.sphereBody.quaternion.x = 0
-        this.sphereBody.quaternion.z = 0
-        // copy position and rotation
-        this.sphere.position.copy(this.sphereBody.position)
-        this.sphere.quaternion.copy(this.sphereBody.quaternion)
-        // set camera position to be at player
-        window.camera.position.copy(this.sphereBody.position)
-
-        this.sphereBody.velocity = v
-
-        if (!this.sphereBody.inJump) {
-            this.sphereBody.linearDamping = 0.6
-        } else {
-            this.sphereBody.linearDamping = 0.2
-        }
-    }
-
-    handleKeypress(event) {
-        // Ignore keypresses typed into a text box
-        if (event.target.tagName === 'INPUT') {
-            return;
-        }
-
-        // The vectors tom which each key code in this handler maps. (Change these if you like)
+        // handle movement
         const up = new THREE.Vector3(0, 1, 0)
         let cameraDirection = new THREE.Vector3()
         window.camera.getWorldDirection(cameraDirection)
@@ -85,29 +80,66 @@ class Player extends Group {
         const right = left.clone().negate()
 
         let jumpMultiplier = 1
-        if (this.sphereBody.inJump) {
+        if (this.physicsBody.inJump) {
             jumpMultiplier = 0.5
         }
-        const v = 50 * this.sphereBody.mass * jumpMultiplier
-        switch (event.code) {
-            case 'KeyW':
-                this.sphereBody.applyForce(forward.clone().multiplyScalar(v), this.sphereBody.position)
-                break
-            case 'KeyS':
-                this.sphereBody.applyForce(backward.clone().multiplyScalar(v), this.sphereBody.position)
-                break
-            case 'KeyA':
-                this.sphereBody.applyForce(left.clone().multiplyScalar(v), this.sphereBody.position)
-                break
-            case 'KeyD':
-                this.sphereBody.applyForce(right.clone().multiplyScalar(v), this.sphereBody.position)
-                break
-            case 'Space':
-                let EPS = 0.1
-                if (this.sphereBody.inJump) {return}
-                this.sphereBody.inJump = true
-                this.sphereBody.applyImpulse(up.clone().multiplyScalar(v * 0.15), this.sphereBody.position)
-                break
+
+        // regulates speed when multiple directions are pressed 
+        let movementDirections = 0
+        Object.keys(this.controller).forEach(key=> {
+            if (this.controller[key].pressed) {
+                movementDirections += 1;
+            }
+        })
+        if (this.controller["Space"].pressed) {
+            movementDirections -= 1;
+        }
+        let movementMultiplier = 1
+        if (movementDirections > 0) {
+            movementMultiplier = (1 / Math.sqrt(movementDirections))
+        }
+
+        const f = 50 * this.physicsBody.mass * jumpMultiplier * movementMultiplier;
+        
+        if (this.controller["KeyW"].pressed) {
+            this.physicsBody.applyForce(forward.clone().multiplyScalar(f), this.physicsBody.position)
+        }
+        if (this.controller["KeyS"].pressed) {
+            this.physicsBody.applyForce(backward.clone().multiplyScalar(f), this.physicsBody.position)
+        }
+        if (this.controller["KeyA"].pressed) {
+            this.physicsBody.applyForce(left.clone().multiplyScalar(f), this.physicsBody.position)
+        }
+        if (this.controller["KeyD"].pressed) {
+            this.physicsBody.applyForce(right.clone().multiplyScalar(f), this.physicsBody.position)
+        }
+        if (this.controller["Space"].pressed) {
+            let EPS = 0.1
+            if (this.physicsBody.inJump) {return} 
+            this.physicsBody.inJump = true
+            this.physicsBody.applyImpulse(up.clone().multiplyScalar(f * 0.10), this.physicsBody.position)
+        }
+
+        let v = this.physicsBody.velocity.clone()
+        // always look where the camera points
+        this.physicsBody.quaternion.setFromAxisAngle(new CANNON.Vec3(0,1,0), window.camera.quaternion.y);
+
+        // this.physicsBody.quaternion.copy(window.camera.quaternion)
+        // this.physicsBody.quaternion.x = 0
+        // this.physicsBody.quaternion.z = 0
+
+        // copy position and rotation
+        this.sphere.position.copy(this.physicsBody.position)
+        this.sphere.quaternion.copy(this.physicsBody.quaternion)
+        // set camera position to be at player
+        // window.camera.position.copy(this.physicsBody.position)
+
+        this.physicsBody.velocity = v
+
+        if (!this.physicsBody.inJump) {
+            this.physicsBody.linearDamping = 0.8
+        } else {
+            this.physicsBody.linearDamping = 0.3
         }
     }
 }
